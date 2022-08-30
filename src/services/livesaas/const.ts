a90b2e61a9b814597febb1b3d27ed31c3bc2f64a
徐小武 <xuxiaowu.xiaowuxu@bytedance.com>
2022-08-30 16:43:03 +0800

    feat: 海外域名问题

--- src/services/livesaas/const.ts
+++ src/services/livesaas/const.ts
@@ -8,5 +8,5 @@ export const Domain = {
   boe: "volcengineapi-boe.byted.org",
   boei18n: "byteplusapi-boei18n.byted.org",
   "cn-north-1": "livesaas.volcengineapi.com",
-  "ap-singapore-1": "livesaas.byteplusapi.com",
+  "ap-singapore-1": "open.byteplusapi.com",
 };
