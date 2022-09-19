42300dc98ec9a4abda3e4181b2d276a04cc3a008
袁圆 <yuanyuan.wallace@bytedance.com>
2022-09-19 14:41:10 +0800

    feat: rtc openapi

--- src/services/rtc/index.ts
+++ src/services/rtc/index.ts
@@ -1,32 +1,40 @@
 import Service from "../../base/service";
 import { ServiceOptions } from "../../base/types";
 import {
-  ListRoomsParams,
-  ListRoomsResult,
-  ListIndicatorsParams,
-  ListIndicatorsResult,
+  StartRecordParams,
+  StartRecordResult,
+  StopRecordParams,
+  StopRecordResult,
+  GetRecordTaskParams,
+  GetRecordTaskResult
 } from "./types";
 
 export class RtcService extends Service {
   constructor(options?: ServiceOptions) {
     super({
       ...options,
-      defaultVersion: "2018-01-01",
+      defaultVersion: "2022-06-01",
       serviceName: "rtc",
     });
   }
 
-  ListRooms = this.createAPI<ListRoomsParams, ListRoomsResult>("ListRooms", {
-    method: "GET",
-    contentType: "json",
-    Version: "2020-12-01",
-    queryKeys: ["AppId"],
-  });
-
-  ListIndicators = this.createAPI<ListIndicatorsParams, ListIndicatorsResult>("ListIndicators", {
+  StartRecord = this.createAPI<StartRecordParams, StartRecordResult>("StartRecord", {
     method: "POST",
     contentType: "json",
-    Version: "2020-12-01",
+    Version: "2022-06-01",
+  });
+
+  StopRecord = this.createAPI<StopRecordParams, StopRecordResult>("StopRecord", {
+    method: "POST",
+    contentType: "json",
+    Version: "2022-06-01",
+  });
+
+  GetRecordTask = this.createAPI<GetRecordTaskParams, GetRecordTaskResult>("GetRecordTask", {
+    method: "GET",
+    contentType: "json",
+    Version: "2022-06-01",
+    queryKeys: ["AppId","RoomId","TaskId"],
   });
 }
 
--- test/rtc.test.js
+++ test/rtc.test.js
@@ -1,25 +1,52 @@
 import { rtcOpenapi } from "../lib";
-import { ListRoomsReponseSchemaValidate, ListIndicatorsReponseSchemaValidate } from "./schema/rtc";
+import { GetRecordTaskParamsSchemaValidate, StopRecordParamsSchemaValidate, StartRecordParamsSchemaValidate } from "./schema/rtc";
 
 const rtcOpenapiService = rtcOpenapi.defaultService;
 
-test("rtc:ListRooms", async () => {
-  const response = await rtcOpenapiService.ListRooms({
-    AppId: "6110e5394f69bb00ca885806",
+test("rtc:StartRecordParams", async () => {
+  const response = await rtcOpenapiService.StartRecordParams({
+    AppId : "Your_AppId",
+    BusinessId : "Your_BusinessId",
+    RoomId : "Your_RoomId",
+    TaskId: "Your_TaskId",
+    RecordMode: 0,
+    FileFormatConfig: {
+      FileFormat: ["HLS", "FLV"]
+    },
+    FileNameConfig: {
+      Prefix: ["directory1", "directory2"],
+      Pattern: ""
+    },
+    StorageConfig: {
+      Type: 0,
+      TosConfig: {
+          UserAccountId: "Your_UserAccountId",
+          Region: "Your_Region",
+          Bucket: "Your_Bucket"
+      }
+    }
   });
-  const validateResult = ListRoomsReponseSchemaValidate(response);
+  const validateResult = StartRecordParamsSchemaValidate(response);
   expect(validateResult).toBe(true);
 });
 
-test("rtc:ListIndicators", async () => {
-  const response = await rtcOpenapiService.ListIndicators({
-    AppId: "6110e5394f69bb00ca885806",
-    StartTime: "2021-07-24T08:00:00+08:00",
-    EndTime: "2021-07-28T08:00:00+08:00",
-    Indicator: "NetworkTransDelay",
-    OS: "mac",
-    Network: "wifi",
+test("rtc:StopRecord", async () => {
+  const response = await rtcOpenapiService.StopRecord({
+    AppId : "Your_AppId",
+    RoomId : "Your_RoomId",
+    TaskId: "Your_TaskId"
   });
-  const validateResult = ListIndicatorsReponseSchemaValidate(response);
+  const validateResult = StopRecordParamsSchemaValidate(response);
+  expect(validateResult).toBe(true);
+});
+
+
+test("rtc:GetRecordTask", async () => {
+  const response = await rtcOpenapiService.GetRecordTask({
+    AppId : "Your_AppId",
+    RoomId : "Your_RoomId",
+    TaskId: "Your_TaskId"
+  });
+  const validateResult = GetRecordTaskParamsSchemaValidate(response);
   expect(validateResult).toBe(true);
 });
